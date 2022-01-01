0a24ab09bc554d3b12eb956d653ab24f4448f316
hakan <erik.hakansson96@gmail.com>
2022-01-01 16:40:20 +0100

    readded deleted devices

---
+++ osgate/devices/defaultDevice.py
@@ -0,0 +1,10 @@
+from devices.device import Device, DeviceChannel
+
+class DefaultDevice(Device):
+    def __init__(self, name, uuid, channels):
+        self.name = name
+        self.uuid = uuid
+        self.channels = [DeviceChannel(channel) for channel in channels]
+
+    def ping() -> str:
+        return "pong"
---
+++ osgate/devices/deviceFactory.py
@@ -0,0 +1,20 @@
+from devices.device import Device
+from devices.defaultDevice import DefaultDevice, DeviceChannel
+
+def create_device_channel(channel: dict) -> DeviceChannel:
+    return DeviceChannel(
+        name = channel.get('name'),
+        unit = channel.get('unit'),
+        interval = channel.get('interval'),
+    )
+
+def create_device(device_type: str, device_data: dict) -> Device:
+    match device_type:
+        case "sensor":        
+            return DefaultDevice(
+                name = device_data.get("name"),
+                uuid = device_data.get("uuid"),
+                channels = [create_device_channel(channel) for channel in device_data.get("channels")],
+            )
+        case _:
+            raise ValueError(f"Invalid device type: {device_type}; perhaps there's a typo in the config file.")
