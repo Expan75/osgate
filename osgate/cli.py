3fa3316dcb71da5d68459ba1f88554f9a7427836
hakan <erik.hakansson96@gmail.com>
2022-03-02 11:24:44 +0100

    add version statement

--- osgate/cli.py
+++ osgate/cli.py
@@ -1,5 +1,9 @@
 from argparse import ArgumentParser
 
+__VERSION__ = "0.0.1"
+versioning_message = f"""
+    osgate v{__VERSION__} | simple gateway for use within industrial IoT.
+"""
 
 def parse_args():
     parser = ArgumentParser(description="Argument parser")
@@ -13,4 +17,5 @@ def parse_args():
     parser.add_argument(
         "--debug", "-d", dest="debug", action="store_true", help="Activate debug log"
     )
+    parser.add_argument("-v", "--version", action="version", version=versioning_message)
     return parser.parse_args()
